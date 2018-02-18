80f44b0e1ef9bcae107b3353652b16cd795e2dd4
yurist38 <yurist38@mail.ru>
2018-02-18 16:24:46 +0100

    Change port, add GET endpoint

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import express from 'express';
 import transpiler from 'ascii-text-generator';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send(`To get the result access "${req.get('host')}/generate" endpoint.`);
+});
+
 app.post('/generate', (req, res) => {
-  const {text, style} = req.params;
+  const {text, style} = req.body;
+
+  if (!text) {
+    res.json({text: 'Error: seems like text is empty...'});
+    return;
+  }
+
   const inputText = text.replace(/\W+/g, '');
 
   try {
-    const transpiledText = transpiler(inputText, style);
+    const transpiledText = transpiler(inputText, style || "2");
     res.json({text: transpiledText});
   } catch(e) {
     res.json({text: e});
   }
 });
 
-app.listen(3838);
+app.listen(80);
